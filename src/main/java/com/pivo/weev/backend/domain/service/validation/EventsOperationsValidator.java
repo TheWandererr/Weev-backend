74e60d9a737cc05d633552df98af4161be8794e3
TheWandererr <kartem2013@yandex.by>
2023-09-23 18:16:00 +0300

    event start time at least +2 hours

--- src/main/java/com/pivo/weev/backend/domain/service/validation/EventsOperationsValidator.java
+++ src/main/java/com/pivo/weev/backend/domain/service/validation/EventsOperationsValidator.java
@@ -6,6 +6,7 @@ import static com.pivo.weev.backend.domain.utils.Constants.ValidatableFields.LOC
 import static com.pivo.weev.backend.domain.utils.Constants.ValidatableFields.LOCAL_START_DATE_TIME;
 import static java.lang.String.format;
 import static java.time.Instant.now;
+import static java.time.temporal.ChronoUnit.HOURS;
 
 import com.pivo.weev.backend.domain.model.event.CreatableEvent;
 import com.pivo.weev.backend.domain.model.exception.ReasonableException;
@@ -21,7 +22,7 @@ public class EventsOperationsValidator {
 
     private void validateDateTimes(CreatableEvent validatable) {
         Instant startInstant = toInstant(validatable.getLocalStartDateTime(), validatable.getStartTimeZoneId());
-        if (startInstant.isBefore(now())) {
+        if (startInstant.isBefore(now().plus(2, HOURS))) {
             throw new ReasonableException(format(FIELD_VALIDATION_FAILED_ERROR_PATTERN, LOCAL_START_DATE_TIME));
         }
         Instant endInstant = toInstant(validatable.getLocalEndDateTime(), validatable.getEndTimeZoneId());
