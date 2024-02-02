d1f4a009ff617543cd60c0143f3da0fb29e2d62d
TheWandererr <kartem2013@yandex.by>
2024-02-03 01:58:47 +0300

    templates by verification scope

--- src/main/java/com/pivo/weev/backend/domain/service/message/MailMessageFactory.java
+++ src/main/java/com/pivo/weev/backend/domain/service/message/MailMessageFactory.java
@@ -3,9 +3,11 @@ package com.pivo.weev.backend.domain.service.message;
 import static com.pivo.weev.backend.domain.utils.Constants.Messaging.EmailMessages.EMAIL_CHANGE_PASSWORD_SUBJECT;
 import static com.pivo.weev.backend.domain.utils.Constants.Messaging.EmailMessages.EMAIL_VERIFICATION_REQUEST_SUBJECT;
 import static com.pivo.weev.backend.domain.utils.Constants.Messaging.Templates.EMAIL_CHANGE_PASSWORD_FTL;
+import static com.pivo.weev.backend.domain.utils.Constants.Messaging.Templates.VERIFICATION_TEMPLATES_MAPPING;
 
 import com.pivo.weev.backend.config.messaging.MessageBundle;
 import com.pivo.weev.backend.config.messaging.TemplatesBundle;
+import com.pivo.weev.backend.domain.model.auth.VerificationScope;
 import com.pivo.weev.backend.domain.model.messaging.MailMessage;
 import com.pivo.weev.backend.domain.model.messaging.source.ChangePasswordSource;
 import com.pivo.weev.backend.domain.model.messaging.source.EmailVerificationSource;
@@ -19,10 +21,11 @@ public class MailMessageFactory {
     private final MessageBundle messageBundle;
     private final TemplatesBundle templatesBundle;
 
-    public MailMessage buildVerificationMessage(String email, String template, EmailVerificationSource source) {
+    public MailMessage buildVerificationMessage(String email, VerificationScope verificationScope, EmailVerificationSource source) {
         MailMessage message = new MailMessage();
         String subject = messageBundle.getEmailMessage(EMAIL_VERIFICATION_REQUEST_SUBJECT);
         message.setSubject(subject);
+        String template = VERIFICATION_TEMPLATES_MAPPING.get(verificationScope);
         String content = templatesBundle.getTemplateContent(template, source);
         message.setContent(content);
         message.setRecipient(email);
--- src/main/java/com/pivo/weev/backend/domain/service/message/MailMessagingService.java
+++ src/main/java/com/pivo/weev/backend/domain/service/message/MailMessagingService.java
@@ -1,7 +1,5 @@
 package com.pivo.weev.backend.domain.service.message;
 
-import static com.pivo.weev.backend.domain.utils.Constants.Messaging.Templates.VERIFICATION_TEMPLATES_MAPPING;
-
 import com.pivo.weev.backend.domain.model.auth.VerificationScope;
 import com.pivo.weev.backend.domain.model.messaging.MailMessage;
 import com.pivo.weev.backend.domain.model.messaging.source.ChangePasswordSource;
@@ -19,8 +17,7 @@ public class MailMessagingService {
 
     @Async(value = "mailExecutor")
     public void sendVerificationMessage(String email, VerificationScope verificationScope, EmailVerificationSource source) {
-        String template = VERIFICATION_TEMPLATES_MAPPING.get(verificationScope);
-        MailMessage message = mailMessageFactory.buildVerificationMessage(email, template, source);
+        MailMessage message = mailMessageFactory.buildVerificationMessage(email, verificationScope, source);
         mailSender.sendHtmlMessage(message);
     }
 
